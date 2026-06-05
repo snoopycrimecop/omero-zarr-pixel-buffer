fbfa684bb8e3056c1b3172f644801e435a91c948
Kevin Kozlowski <kevin@glencoesoftware.com>
2026-06-05 15:10:17 -0400

    checkstyle

--- src/main/java/com/glencoesoftware/omero/zarr/ZarrStore.java
+++ src/main/java/com/glencoesoftware/omero/zarr/ZarrStore.java
@@ -38,9 +38,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-
 import org.slf4j.LoggerFactory;
-
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
